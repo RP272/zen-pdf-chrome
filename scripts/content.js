f71fd2282a520edb0accb0c6f02e3025efc7e76c
RP272 <272931@student.pwr.edu.pl>
2026-07-07 10:51:24 +0200

    zen mode for PDF files served over HTTP or HTTPS

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,34 @@
-let toolbarVisible = true;
+if (location.pathname.toLowerCase().endsWith(".pdf")) {
 
-// Callback function to handle 'toggle-toolbar' message from background.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "toggle-toolbar") {
-        const shadowRoot = chrome.dom.openOrClosedShadowRoot(document.body);
-        
-        if (!shadowRoot) {
-            console.error("Can't access Shadow DOM");
-            return;
+    let toolbarVisible = true;
+
+    // Callback function to handle 'toggle-toolbar' message from background.js
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.action === "toggle-toolbar") {
+            const shadowRoot = chrome.dom.openOrClosedShadowRoot(document.body);
+
+            if (!shadowRoot) {
+                console.error("Can't access Shadow DOM");
+                return;
+            }
+
+            const pdfIframe = shadowRoot.querySelector("iframe");
+
+            if (!pdfIframe) {
+                console.error("Couldn't find PDF iframe.");
+                return;
+            }
+
+            if (toolbarVisible === true) {
+                // I think this is the only way to hide the toolbar, since the contents are stored in the iframe and we can't access them directly
+                pdfIframe.style.transform = "translateY(-56px)";
+                pdfIframe.style.height = "calc(100vh + 56px)";
+                toolbarVisible = false;
+            } else {
+                pdfIframe.style.transform = "";
+                pdfIframe.style.height = "";
+                toolbarVisible = true;
+            }
         }
-
-        const pdfIframe = shadowRoot.querySelector("iframe");
-
-        if (!pdfIframe) {
-            console.error("Couldn't find PDF iframe.");
-            return;
-        }
-
-        if (toolbarVisible === true) {
-            // I think this is the only way to hide the toolbar, since the contents are stored in the iframe and we can't access them directly
-            pdfIframe.style.transform = "translateY(-56px)";
-            pdfIframe.style.height = "calc(100vh + 56px)";
-            toolbarVisible = false;
-        } else {
-            pdfIframe.style.transform = "";
-            pdfIframe.style.height = "";
-            toolbarVisible = true;
-        }
-    }
-});
+    });
+}
